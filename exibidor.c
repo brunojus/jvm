7ada11a56bced988e9f229fbcda5dabf8edbe68e
William <williamsimiao@gmail.com>
2018-05-13 21:32:56 -0300

    trocando ultimos fout por arq

--- exibidor.c
+++ exibidor.c
@@ -7,26 +7,26 @@ int bit_is_true(int code, int id) {
     return code & (1 << id);
 }
 /*De acordo com tabela em https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.1-200-E.1*/
-void print_access_flags(int code, FILE* fout) {
-    fprintf(fout, "[");
-    if (bit_is_true(code, 0)) fprintf(fout, "public ");
-    else if (bit_is_true(code, 1)) fprintf(fout, "private ");
-    else if (bit_is_true(code, 2)) fprintf(fout, "protected ");
+void print_access_flags(int code, FILE* arq) {
+    fprintf(arq, "[");
+    if (bit_is_true(code, 0)) fprintf(arq, "public ");
+    else if (bit_is_true(code, 1)) fprintf(arq, "private ");
+    else if (bit_is_true(code, 2)) fprintf(arq, "protected ");
 
-    if (bit_is_true(code, 3)) fprintf(fout, "static ");
-    if (bit_is_true(code, 4)) fprintf(fout, "final ");
-    if (bit_is_true(code, 5)) fprintf(fout, "super ");
-    if (bit_is_true(code, 6)) fprintf(fout, "volatile ");
-    if (bit_is_true(code, 7)) fprintf(fout, "transient ");
-    if (bit_is_true(code, 8)) fprintf(fout, "native ");
-    if (bit_is_true(code, 9)) fprintf(fout, "interface ");
-    if (bit_is_true(code, 10)) fprintf(fout, "abstract ");
-    fprintf(fout, "]");
+    if (bit_is_true(code, 3)) fprintf(arq, "static ");
+    if (bit_is_true(code, 4)) fprintf(arq, "final ");
+    if (bit_is_true(code, 5)) fprintf(arq, "super ");
+    if (bit_is_true(code, 6)) fprintf(arq, "volatile ");
+    if (bit_is_true(code, 7)) fprintf(arq, "transient ");
+    if (bit_is_true(code, 8)) fprintf(arq, "native ");
+    if (bit_is_true(code, 9)) fprintf(arq, "interface ");
+    if (bit_is_true(code, 10)) fprintf(arq, "abstract ");
+    fprintf(arq, "]");
 }
 
 /*Print do CAFEBABE em hexa*/
-void print_magic(ClassFile* cf, FILE* fout) {
-    fprintf(fout, "MAGIC: %x\n", cf->magic);
+void print_magic(ClassFile* cf, FILE* arq) {
+    fprintf(arq, "MAGIC: %x\n", cf->magic);
 }
 
 /*Imprime os valores lidos pela função load_versions em "leitor"*/
